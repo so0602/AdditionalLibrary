ff33530fc78e875e1a946819591479b15a28d8ea
FreddySo <freddyso0602@gmail.com>
2012-07-10 16:36:33 +0800

    Add Zoom Control

--- MySources/MKMapView+Addition.h
+++ MySources/MKMapView+Addition.h
@@ -5,3 +5,9 @@
 -(void)removeAllAnnotations; //With out User Location
 
 @end
+
+@interface MKMapView (ZoomLevel)
+
+-(void)setCenterCoordinate:(CLLocationCoordinate2D)centerCoordinate zoomLevel:(NSUInteger)zoomLevel animated:(BOOL)animated;
+
+@end
